c70094fb148512693c7f47484ccd8e19bb0f9063
David Sveningsson <david.sveningsson@forsakringskassan.se>
2025-09-15 01:35:41 +0200

    chore(deps): update @forsakringskassan/eslint monorepo to v13

--- packages/prettier-config-svelte/prettier.cjs
+++ packages/prettier-config-svelte/prettier.cjs
@@ -6,6 +6,7 @@ const { spawn } = require("child_process");
 const pkgPath = path.dirname(require.resolve("prettier/package.json"));
 const binary = path.join(pkgPath, "bin/prettier.cjs");
 
+/* eslint-disable-next-line sonarjs/no-os-command-from-path -- want to execute node from PATH */
 spawn("node", [binary, ...process.argv.slice(2)], {
     stdio: "inherit",
 }).on("exit", (code) => {
